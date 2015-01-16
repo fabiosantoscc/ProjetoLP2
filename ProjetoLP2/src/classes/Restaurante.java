da56fed8c1a9e712ef4971bffaaab4a66eb08ffb
RonnanSouza <ronan.souza@ccc.ufcg.edu.br>
2015-01-15 23:48:26 -0300

    Conclusão parcial da classe Restaurante

--- ProjetoLP2/src/classes/Restaurante.java
+++ ProjetoLP2/src/classes/Restaurante.java
@@ -3,33 +3,68 @@ package classes;
 /**
  * Classe que representa um restaurante
  * 
- * @author -
+ * @author Ronan Souza
  * Ultima atualizacao 14/01/2015 / Fabio Alexandre
  */
 
 public class Restaurante implements Servicos {
+	
+	private double consumoGeral, consumoUnidade1, consumoUnidade2;
+	private int quantUsoUnidade1, quantUsoUnidade2;
+	
 
-	private double valorConsumido;
-
-	public double getValorConsumido() {
-		return valorConsumido;
+	public  double getConsumoUnidade1(){
+		return consumoUnidade1;
 	}
 
-	public void setValorConsumido(double valorConsumido) {
-		this.valorConsumido = valorConsumido;
+	public double getConsumoUnidade2() {
+		return consumoUnidade2;
 	}
-
+	
+	public void AdicionaNovoConsumo(String unidade, double valor) throws Exception{
+		if (!(unidade.equals("Térreo")) || unidade.equals("Cobertura")){
+			throw new Exception("Unidade do restaurante inválida!!");
+		}
+		consumoGeral += valor;
+		switch (unidade) {
+		case "Térreo":
+			quantUsoUnidade1++;
+			consumoUnidade1 += valor;
+			break;
+		case "Cobertura":
+			quantUsoUnidade2++;
+			consumoUnidade2 += valor;
+			break;
+		}
+		
+	}
 	@Override
 	public double getPreco() {
-		return 0;
+		return consumoGeral;
 	}
 	
 	public String toStrig() {
-		return "Restaurante [ valorConsumido = " + valorConsumido + " ]";
+		return "Restaurante:"
+				+ "\nUnidade 1 (Térreo)"
+				+ "     \n Quantidade de usos: "+quantUsoUnidade1
+				+ "     \n Valor consumido: "+ consumoUnidade1
+				+ "\nUnidade 2 (Cobertura)"
+				+ "     \n Quantidade de usos: "+quantUsoUnidade2
+				+ "     \n Valor consumido: "+ consumoUnidade2
+				+ "\n\nValor total: "+consumoGeral;
 	}
 	
 	@Override
 	public boolean equals( Object obj ) {
+		if (obj instanceof Restaurante){
+			return false;
+		}
+		Restaurante novo = (Restaurante) obj;
+		
+		if (novo.getConsumoUnidade1() == consumoUnidade1 &&
+				novo.getConsumoUnidade2() == consumoUnidade2 && novo.getPreco() == consumoGeral){
+			return true;
+		}
 		return false;
 	}
 }
