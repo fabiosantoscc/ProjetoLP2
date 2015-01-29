acd004c3ee404bbc27128d3c6d01b758a6d00d61
fabio <fabio.junior@ccc.ufcg.edu.br>
2015-01-29 18:25:32 -0200

    Modificações em hotel.

--- ProjetoLP2/src/classes/Hotel.java
+++ ProjetoLP2/src/classes/Hotel.java
@@ -12,7 +12,8 @@ import java.util.HashMap;
 
 public class Hotel {
 
-	private static int camaExtra = 10;
+	private double notaDeAceitacao = 0;
+	private static int camaExtra = 20;
 	private static int quartoPresidencial = 5;
 	private static int quartoExecutivoSimples = 5;
 	private static int quartoExecutivoDuplo = 15;
@@ -21,12 +22,22 @@ public class Hotel {
 	private static int quartoLuxoDuplo = 15;
 	private static int quartoLuxoTriplo = 20;
 	
+	private List<Opiniao> opinioes = new ArrayList<Opiniao>();
+	
 	// map com um hospede como chave, e um List<Contrato> como valor
 	
 	HashMap<Hospede, List<Contrato>> hospedes = new HashMap<Hospede, List<Contrato>>();
 	
+	/**
+	 * 
+	 */
+	
 	public void checkin() {
 	}
+	
+	/**
+	 * 
+	 */
 	
 	public void checkout() {
 	}
@@ -42,9 +53,52 @@ public class Hotel {
 		hospedes.put(hospede, contratos);
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
+	
+	public double getNotaDeAceitacao() {
+		return notaDeAceitacao;
+	}
+	
+	/**
+	 * 
+	 * @param opinioes
+	 */
+	
+	public void calculaNotaDeAceitacao( List<Opiniao> opinioes ) {
+		int soma = 0;
+		for ( int i = 0; i < opinioes.size(); i++) {
+			soma += opinioes.get(i).getNota();
+		}
+		
+		double nota = soma / opinioes.size();
+		this.notaDeAceitacao = nota;
+	}
+	
+	/**
+	 * 
+	 * @param umaOpiniao
+	 */
+	
+	public void adicionaOpiniao( Opiniao umaOpiniao ) {
+		this.opinioes.add(umaOpiniao);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	
 	public static int getCamaExtra() {
 		return camaExtra;
 	}
+	
+	/**
+	 * 
+	 * @param camaExtra
+	 */
 	
 	public static void setCamaExtra(int camaExtra) {
 		Hotel.camaExtra = camaExtra;
