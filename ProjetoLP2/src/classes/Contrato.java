9bd129aa622ee6df4e3beeb9e7d5b213ea06c2fa
Fabio <fabio.junior@ccc.ufcg.edu.br>
2015-02-04 16:54:40 -0200

    -

--- ProjetoLP2/src/classes/Contrato.java
+++ ProjetoLP2/src/classes/Contrato.java
@@ -16,38 +16,19 @@ public class Contrato {
 	private List<Servicos> servicos = new ArrayList<Servicos>();
 	private boolean fechado;
 	private double valorAPagar;
-	
-	/**
-	 * Construtor de um contrato que nao tem data de encerramento
-	 * 
-	 * @param fechado Indica se o contrato ja foi ou nao pago, esta fechado ou aberto
-	 * @param inicioDoContrato Data do inicio do contrato
-	 * @param fimDoContrato Data do fim do contrato
-	 */
+	private int numeroDeNoites;
 	
 	public Contrato( Hospede hospede ) {
 		this.fechado = false;
 	}
 	
-	/**
-	 * 
-	 */
-	
 	public void adicionaServico( Servicos servico ) {
 		servicos.add(servico);
 	}
 
-	/**
-	 * @return Verdadeiro ou falso dependendo do contrato se estiver aberto ou fechado
-	 */
-	
 	public boolean isfechado() {
 		return fechado;
 	}
-	
-	/**
-	 *
-	 */
 	
 	public void SetFechado(boolean fechado) {
 		this.fechado = fechado;
@@ -57,29 +38,19 @@ public class Contrato {
 	 * @return Todo o valor gasto durante o periodo
 	 */
 	
-	public double getValorAPagarl(){
+	public double getValorAPagar(){
 		return valorAPagar;
 	}
-	
-	/**
-	 * Retorna todos os dados referentes a classe em uma string
-	 */
 	
 	@Override
 	public String toString() {
 		return "";
 	}
 	
-	/**
-	 * Verifica se dois contratos sao iguais
-	 */
-	
 	@Override
 	public boolean equals( Object obj ) {
 		if (!(obj instanceof Contrato))
 			return false;
-		
-		Contrato novoContrato = (Contrato)obj;
 		
 		return true;
 	}
