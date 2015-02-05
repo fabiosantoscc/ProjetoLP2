02b7b4cc8725f987d4fb040dec6df317169bad1b
RonnanSouza <ronan.souza@ccc.ufcg.edu.br>
2015-02-05 09:08:16 -0300

    Edição do painel de interface Grafica BuscarUsuário **Passivel de discussão

--- ProjetoLP2/src/InterfaceGrafica/AdicionarHospede.java
+++ ProjetoLP2/src/InterfaceGrafica/AdicionarHospede.java
@@ -2,6 +2,9 @@ package InterfaceGrafica;
 
 import javax.swing.JPanel;
 import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
 
 public class AdicionarHospede extends JPanel {
 
@@ -9,8 +12,28 @@ public class AdicionarHospede extends JPanel {
 	 * Create the panel.
 	 */
 	public AdicionarHospede() {
-		setBackground(Color.BLUE);
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Buscar por nome");
+		lblNewLabel.setBounds(41, 206, 161, 28);
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Buscar por CPF");
+		lblNewLabel_1.setBounds(41, 265, 139, 15);
+		add(lblNewLabel_1);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(178, 213, 179, 21);
+		add(textPane);
+		
+		JTextPane textPane_1 = new JTextPane();
+		textPane_1.setBounds(178, 259, 179, 21);
+		add(textPane_1);
+		
+		JButton btnNewButton = new JButton("Buscar");
+		btnNewButton.setBounds(399, 350, 117, 25);
+		add(btnNewButton);
+		
 
 	}
-
 }
