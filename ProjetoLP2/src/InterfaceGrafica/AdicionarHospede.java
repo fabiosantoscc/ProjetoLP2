f60d668e8f56cb9d74ef0db2bab91607ece9c3d0
RonnanSouza <ronan.souza@ccc.ufcg.edu.br>
2015-02-05 09:12:29 -0300

    Correções nas Telas BuscarHospedes, AdicionarHospedes e MenuPrincipal

--- ProjetoLP2/src/InterfaceGrafica/AdicionarHospede.java
+++ ProjetoLP2/src/InterfaceGrafica/AdicionarHospede.java
@@ -3,37 +3,21 @@ package InterfaceGrafica;
 import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JLabel;
-import javax.swing.JTextPane;
-import javax.swing.JButton;
 
-public class AdicionarHospede extends JPanel {
+public class BuscarHospede extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
-	public AdicionarHospede() {
+	public BuscarHospede() {
+		setBackground(Color.YELLOW);
+		setForeground(Color.YELLOW);
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Buscar por nome");
-		lblNewLabel.setBounds(41, 206, 161, 28);
-		add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Buscar por CPF");
-		lblNewLabel_1.setBounds(41, 265, 139, 15);
-		add(lblNewLabel_1);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(178, 213, 179, 21);
-		add(textPane);
-		
-		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setBounds(178, 259, 179, 21);
-		add(textPane_1);
-		
-		JButton btnNewButton = new JButton("Buscar");
-		btnNewButton.setBounds(399, 350, 117, 25);
-		add(btnNewButton);
-		
+		JLabel label = new JLabel("New label");
+		label.setBackground(Color.YELLOW);
+		label.setBounds(193, 149, 200, 50);
+		add(label);
 
 	}
 }
