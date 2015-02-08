af9bf6c8b8ef0f36ecf24194cbb1c0f8dc0f4a01
Ronan <Ronan@Ronan-PC>
2015-02-08 10:34:07 -0200

    Mudanças na classe Adicionar Contratos

--- ProjetoLP2/src/InterfaceGrafica/ContratosDoHospede.java
+++ ProjetoLP2/src/InterfaceGrafica/ContratosDoHospede.java
@@ -1,15 +1,50 @@
 package InterfaceGrafica;
 
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 
 import classes.Hospede;
+
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class ContratosDoHospede extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
+	ArrayList<String> list = new ArrayList<String>(); 
 	public ContratosDoHospede(Hospede hospedeAtual) {
+		setLayout(null);
+		
+		list.add("Fabio");
+		list.add("Ronan");
+		list.add("Ravi");
+		list.add("Jaziel");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		list.add("Projeto");
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"ronan", "souza"}));
+		comboBox.setBounds(137, 76, 110, 20);
+		add(comboBox);
 
 	}
 
