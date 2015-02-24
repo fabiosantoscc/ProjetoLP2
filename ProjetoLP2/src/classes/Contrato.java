1dccbdaba7b54eac9931ee258d6b5ef71230274b
Fabio <fabio.junior@ccc.ufcg.edu.br>
2015-02-24 19:19:05 -0300

    -

--- ProjetoLP2/src/classes/Contrato.java
+++ ProjetoLP2/src/classes/Contrato.java
@@ -1,177 +1,198 @@
 package classes;
 
-/**
- * Classe que representa um contrato
- * 
- * @author Fabio Alexandre Santos Silva Júnior
- * @date 02/02/2015
- * Ultima alteracao: 08/02/2015
- * 
- */
+import excecoes.NumeroDeNoitesInvalidoException;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import java.util.ArrayList;
-
-import excecoes.*;
 
 /**
- * Classe que manipula os dados referentes ao contrato do hospede.
- * @author Ravi Leite
- * @data 12/01/2015
- * Ultima alteracao: 05/02/2015 / Jaaziel Moura
+ * Classe que representa um contrato.
+ * 
+ *     @author Fabio Alexandre Santos Silva Júnior
+ *     @date 02/02/2015
+ *     Ultima alteracao: 24/02/2015 / Fabio Alexandre
  */
 
 public class Contrato implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	private List<Servicos> servicos = new ArrayList<Servicos>();
-	private boolean aberto;
-	private double valorAPagar;
-	private int numeroDeNoites;
-	private int diaInicial, mesInicial, anoInicial;
-	private Calendar data;
-	private EstrategiaDeCalculoDaMontante e;
-	
-	public Contrato( int numeroDeNoites ) throws NumeroDeNoitesInvalidoException {
-		Calendar c = Calendar.getInstance();
-		this.data = c;
-		diaInicial = c.get(Calendar.DAY_OF_MONTH);
-		mesInicial = c.get(Calendar.MONTH) + 1;
-		anoInicial = c.get(Calendar.YEAR);
-		
-		if ( numeroDeNoites <= 0)
-			throw new NumeroDeNoitesInvalidoException("Numero de noites deve ser maior que zero.");
-		
-		this.numeroDeNoites = numeroDeNoites;
-		this.aberto = true;
-	}
-	
-	/**
-	 * 
-	 * @param meusServicos
-	 */
-	
-	public void servicosContrato( List<Servicos> meusServicos ) {
-		servicos.addAll(meusServicos);
-	}
-	
-	/**
-	 * 
-	 * @param servico
-	 */
-	
-	public void adicionaServico( Servicos servico ) {
-		servicos.add(servico);
-	}
 
-	/**
-	 * 
-	 * @return
-	 */
-	
-	public int getDiaInicial() {
-		return diaInicial;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	
-	public int getMesInicial() {
-		return mesInicial;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	
-	public int getAnoInicial() {
-		return anoInicial;
-	}
-	
-	public List<Servicos> getServicos(){
-		return servicos;
-	}
-	/**
-	 * 
-	 * @return
-	 */
-	
-	public boolean isAberto() {
-		return aberto;
-	}
-	
-	/**
-	 * 
-	 * @param fechado
-	 */
-	
-	public void setAberto(boolean aberto) {
-		this.aberto = aberto;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	
-	public int getNumeroDeNoites() {
-		return numeroDeNoites;
-	}
-	
-	/**
-	 * @return Todo o valor gasto durante o periodo
-	 */
-	
-	public double getValorAPagar(){
-		return valorAPagar;
-	}
-	
-	/**
-	 * 
-	 */
-	
-	public void calculaTotalAPagar() {
-	}
-	
-	/**
-	 * 
-	 */
-	
-	@Override
-	public String toString() {
-		String representacao = "Contrato - Criado em " + getDiaInicial() + "/" + getMesInicial() + "/" +
-				getAnoInicial() + " as " + data.get(Calendar.HOUR_OF_DAY) + ":" + data.get(Calendar.MINUTE)
-				+ ":" + data.get(Calendar.SECOND);
-		
-		if ( isAberto() )
-			representacao += ", Contrato aberto";
-		else {
-			representacao += ", Contrato fechado";
-		}
-		
-		representacao += ", Noites de hospedagem: " + getNumeroDeNoites();
-		
-		return representacao;
-	}
-	
-	/**
-	 * 
-	 */
-	
-	@Override
-	public boolean equals( Object obj ) {
-		if (!(obj instanceof Contrato))
-			return false;
-		
-		Contrato c = (Contrato) obj;
-		
-		return getNumeroDeNoites() == c.getNumeroDeNoites() && getDiaInicial() == c.getDiaInicial()
-				&& getMesInicial() == c.getMesInicial() && getAnoInicial() == c.getAnoInicial()
-				&& getValorAPagar() == c.getValorAPagar() && isAberto() == c.isAberto();			
-	}
+  private static final long serialVersionUID = 1L;
+  private List<Servicos> servicos = new ArrayList<Servicos>();
+  private boolean aberto;
+  private double valorAPagar;
+  private int numeroDeNoites;
+  private Calendar dataInicial;
+
+  /**
+   * Construtor de um Contrato.
+   * 
+   * @param numeroDeNoites - Numero de noites que o hospede vai ficar no hotel.
+   * @throws NumeroDeNoitesInvalidoException - Numero de noites invalido.
+   */
+  
+  public Contrato( int numeroDeNoites ) throws NumeroDeNoitesInvalidoException {
+    Calendar dataInicial = Calendar.getInstance();
+    this.dataInicial = dataInicial;
+
+    if ( numeroDeNoites <= 0) {
+      throw new NumeroDeNoitesInvalidoException("Numero de noites deve ser maior que zero.");
+    } 
+
+    this.numeroDeNoites = numeroDeNoites;
+    this.aberto = true;
+  }
+
+  /**
+  * Adiciona varios servicos no Contrato.
+  * 
+  * @param meusServicos - Servicos a serem adicionados.
+  */
+
+  public void servicosContrato( List<Servicos> meusServicos ) {
+    servicos.addAll(meusServicos);
+  }
+
+  /**
+  * Adiciona um servico no Contrato.
+  * 
+  * @param servico - Servico a ser adicionado.
+  */
+
+  public void adicionaServico( Servicos servico ) {
+    servicos.add(servico);
+  }
+
+  /**
+  * Recupera o dia inicial do Contrato.
+  * 
+  * @return int - Dia inicial do Contrato.
+  */
+
+  public int getDiaInicial() {
+    return dataInicial.get(Calendar.DAY_OF_MONTH);
+  }
+
+  /**
+  * Recupera o mes inicial do Contrato.
+  * 
+  * @return int - Mes inicial do Contrato.
+  */
+
+  public int getMesInicial() {
+    return dataInicial.get(Calendar.MONTH) + 1;
+  }
+
+  /**
+  * Recupera o ano inicial do Contrato.
+  * 
+  * @return int - Ano inicial do Contrato.
+  */
+
+  public int getAnoInicial() {
+    return dataInicial.get(Calendar.YEAR);
+  }
+
+  /**
+   * Recupera a lista de servicos do contrato.
+   * 
+   * @return List<> - Todos os servicos do Contrato.
+   */
+  
+  public List<Servicos> getServicos() {
+    return servicos;
+  }
+
+  /**
+  * Recupera o estado do Contrato, se eh aberto ou fechado.
+  * 
+  * @return boolean - True se o Contrato estiver aberto, false caso contrario.
+  */
+
+  public boolean isAberto() {
+    return aberto;
+  }
+
+  /**
+  * Atribui um novo estado ao Contrato.
+  * 
+  * @param estadoContrato - Novo estado do Contrato.
+  */
+
+  public void setAberto(boolean estadoContrato) {
+    this.aberto = estadoContrato;
+  }
+
+  /**
+  * Recupera o numero de noites do Contrato.
+  * 
+  * @return int - Numero de noites do Contrato.
+  */
+
+  public int getNumeroDeNoites() {
+    return numeroDeNoites;
+  }
+
+  /**
+  * Recupera o valor a ser pago pelo Contrato.
+  * 
+  * @return double - O valor gasto no geral durante o periodo do Contrato.
+  */
+
+  public double getValorAPagar() {
+    return valorAPagar;
+  }
+
+  /**
+  * Calcula o valor total a pagar no Contrato.
+  */
+
+  public void calculaTotalAPagar() {
+    //implementar
+  }
+
+  /**
+  * Representacao do Contrato em uma String.
+  * 
+  * return String - representacao do Contrato em uma String.
+  */
+
+  @Override
+  public String toString() {
+    String representacao = "Contrato - Criado em " + getDiaInicial() + "/" + getMesInicial() + "/"
+        + getAnoInicial() + " as " + dataInicial.get(Calendar.HOUR_OF_DAY) + ":" 
+        + dataInicial.get(Calendar.MINUTE) + ":" + dataInicial.get(Calendar.SECOND);
+
+    if ( isAberto() ) {
+      representacao += ", Contrato aberto";
+    }  else {
+      representacao += ", Contrato fechado";
+    }
+
+    representacao += ", Noites de hospedagem: " + getNumeroDeNoites();
+
+    return representacao;
+  }
+
+  /**
+  * Verifica se dois contratos sao iguais.
+  *
+  * @return boolean - True se os Contratos forem iguais, false caso contratio.
+  */
+
+  @Override
+  public boolean equals( Object obj ) {
+    if (!(obj instanceof Contrato)) {
+      return false;
+    }
+
+    Contrato contrato = (Contrato) obj;
+
+    return getNumeroDeNoites() == contrato.getNumeroDeNoites()
+        && getDiaInicial() == contrato.getDiaInicial()
+        && getMesInicial() == contrato.getMesInicial()
+        && getAnoInicial() == contrato.getAnoInicial()
+        && getValorAPagar() == contrato.getValorAPagar()
+        && isAberto() == contrato.isAberto();
+  }
 }
