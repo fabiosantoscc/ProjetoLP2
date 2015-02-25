22cba6cf4b405cf83545f3cc3cd623d09df3d25d
Ravi Leite <ravi.lopes@ccc.ufcg.edu.br>
2015-02-24 21:32:54 -0300

    Calculo das estrategias

--- ProjetoLP2/src/classes/Quarto.java
+++ ProjetoLP2/src/classes/Quarto.java
@@ -1,7 +1,6 @@
 package classes;
 
 import classes.Quarto;
-
 import excecoes.QuantidadeDePessoasInvalidaException;
 
 import java.io.Serializable;
@@ -15,7 +14,11 @@ import java.util.Calendar;
  */
 
 public abstract class Quarto implements Servicos, Serializable  {
-
+  
+  private int camasExtra;
+  private double valorDiario;
+  private EstrategiaDeCalculoDaMontante estrategia;
+  private CalendarioDeEstrategias calendario;
   private static final long serialVersionUID = 1L;
   private Calendar data;
   private int quantidadeDePessoas;
@@ -33,33 +36,47 @@ public abstract class Quarto implements Servicos, Serializable  {
 
   public Quarto(int quantidadeDePessoas) {
     Calendar date = Calendar.getInstance();
+    calendario = new CalendarioDeEstrategias();
     data = date;
     diaInicial = date.get(Calendar.DAY_OF_MONTH);
     mesInicial = date.get(Calendar.MONTH) + 1;
     anoInicial = date.get(Calendar.YEAR);
     this.quantidadeDePessoas = quantidadeDePessoas;
+    camasExtra = 0;
   }
 
-//	public void calculaDespesaTotal(int diaEntrada, int diaSaida, int mesEntrada, int mesSaida, double despesaDiaria){
-//		for (int i = mesEntrada; i <= mesSaida; i++){
-//			for (int j = diaEntrada; j <= diaSaida; j ++){
-//				boolean dataValida = calendario.verificaDataValida(j, i);
-//				if (! dataValida){
-//					i = 1;
-//					if (j == 12) {
-//						j = 1;
-//					}
-//					}
-//					else j++;
-//					break;
-//				}
-//			estrategia = calendario.verificaEstrategia(j, i);
-//			despesaTotal += estrategia.calculaMontante(despesaDiaria);
-//			System.out.println(despesaTotal);
-//			}
-//		despesaTotal *= quantidadeDePessoas;
-//		
-//		}
+	public int calculaDespesaTotal(int diaEntrada, int mesEntrada, int diaSaida, int mesSaida, int anoEntrada,
+			int anoSaida)throws Exception{
+		if (!calendario.verificaDataValida(diaEntrada, mesEntrada)) throw new Exception ("O mes e o dia tem que ser valido.");
+		if (!calendario.verificaDataValida(diaSaida, mesSaida)) throw new Exception ("O mes e o dia tem que ser valido.");
+		boolean dataValida = true;
+		int contador = 0;
+		while (dataValida){
+			if (diaEntrada > diaSaida && mesEntrada == mesSaida && anoEntrada == anoSaida) break;
+			if (!(calendario.verificaDataValida(diaEntrada, mesEntrada))){
+				diaEntrada = 1;
+				if (mesEntrada == 12){
+					anoEntrada++;
+					mesEntrada = 1;
+				}
+				else mesEntrada++;
+			}
+			contador++;
+			estrategia = calendario.verificaEstrategia(diaEntrada, mesEntrada);
+			despesaTotal += estrategia.calculaMontante(valorDiario);
+			System.out.println(despesaTotal);
+			diaEntrada++;
+		}
+		return contador;
+	}
+	
+  public void setValorDiario(double diaria){
+	  valorDiario = diaria;
+  }
+	
+  public double getValorDiqario(){
+	  return valorDiario;
+  }
 
   public int getDiaInicial() {
     return diaInicial;
