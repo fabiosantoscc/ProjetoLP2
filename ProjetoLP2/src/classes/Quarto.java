641957d0e70d6e0cf0ae2dd448206c4af1b8f973
Fabio <fabio.junior@ccc.ufcg.edu.br>
2015-03-03 11:15:46 -0300

    Organização do código.

--- ProjetoLP2/src/classes/Quarto.java
+++ ProjetoLP2/src/classes/Quarto.java
@@ -1,7 +1,6 @@
 package classes;
 
 import classes.Quarto;
-import excecoes.CamaExtraEsgotadaException;
 import excecoes.QuantidadeDePessoasInvalidaException;
 
 import java.io.Serializable;
@@ -31,14 +30,13 @@ public abstract class Quarto implements Servicos, Serializable  {
   * construtor de um quarto.
   * 
   * @param quantidadeDePessoas - Quantidade De Pessoas a se hospedarem no quarto.
- * @throws QuantidadeDePessoasInvalidaException 
- * @throws CamaExtraEsgotadaException 
-  * @throws Exception
+  * @throws QuantidadeDePessoasInvalidaException - Pode lancar excecao de
+  *         quantidade de pessoas diferente do especificado.
   */
 
   public Quarto( int quantidadeDePessoas ) throws QuantidadeDePessoasInvalidaException {
     verificaQuantidadeDePessoas(quantidadeDePessoas);
-	Calendar date = Calendar.getInstance();
+    Calendar date = Calendar.getInstance();
     calendario = new CalendarioDeEstrategias();
     data = date;
     diaInicial = date.get(Calendar.DAY_OF_MONTH);
@@ -48,36 +46,72 @@ public abstract class Quarto implements Servicos, Serializable  {
     this.quantidadeDePessoas = quantidadeDePessoas;
   }
   
-  public void calculaDespesaTotal(int diaEntrada, int mesEntrada, int diaSaida, int mesSaida, int anoEntrada,
-			int anoSaida)throws Exception{
-		if (!calendario.verificaDataValida(diaEntrada, mesEntrada)) throw new Exception ("O mes e o dia tem que ser valido.");
-		if (!calendario.verificaDataValida(diaSaida, mesSaida)) throw new Exception ("O mes e o dia tem que ser valido.");
-		boolean dataValida = true;
-		while (dataValida){
-			if (diaEntrada > diaSaida && mesEntrada == mesSaida && anoEntrada == anoSaida) break;
-			if (!(calendario.verificaDataValida(diaEntrada, mesEntrada))){
-				diaEntrada = 1;
-				if (mesEntrada == 12){
-					anoEntrada++;
-					mesEntrada = 1;
-				}
-				else mesEntrada++;
-			}
-			estrategia = calendario.verificaEstrategia(diaEntrada, mesEntrada);
-			despesaTotal += estrategia.calculaMontante(valorDiaria);
-			System.out.println(despesaTotal);
-			diaEntrada++;
-		}
-	}
- 
-  public void setValorDiario(double diaria) {
-	  valorDiaria = diaria;
-  }
-	
-  public double getValorDiaria() {
-	  return valorDiaria;
+  /**
+   * Metodo que calcula a despesa do quarto.
+   * 
+   * @param diaEntrada
+   * @param mesEntrada
+   * @param diaSaida
+   * @param mesSaida
+   * @param anoEntrada
+   * @param anoSaida
+   * @throws Exception
+   */
+  
+  public void calculaDespesaTotal(int diaEntrada, int mesEntrada, int diaSaida, int mesSaida,
+      int anoEntrada, int anoSaida) throws Exception {
+    if (!calendario.verificaDataValida(diaEntrada, mesEntrada)) {
+      throw new Exception("O mes e o dia tem que ser valido.");
+    }
+    
+    if (!calendario.verificaDataValida(diaSaida, mesSaida)) {
+      throw new Exception("O mes e o dia tem que ser valido.");
+    }
+    
+    boolean dataValida = true;
+    while (dataValida) {
+      if (diaEntrada > diaSaida && mesEntrada == mesSaida && anoEntrada == anoSaida) {
+        break;
+      }
+      if (!(calendario.verificaDataValida(diaEntrada, mesEntrada))) {
+        diaEntrada = 1;
+        if (mesEntrada == 12) {
+          anoEntrada++;
+          mesEntrada = 1;
+		} else {
+          mesEntrada++;
+        }
+      }
+      estrategia = calendario.verificaEstrategia(diaEntrada, mesEntrada);
+      despesaTotal += estrategia.calculaMontante(valorDiaria);
+      System.out.println(despesaTotal);
+      diaEntrada++;
+    }
   }
 
+  /**
+   * 
+   * @param diaria
+   */
+ 
+  public void setValorDiario(double diaria) {
+    valorDiaria = diaria;
+  }
+
+  /**
+   * 
+   * @return
+   */
+  
+  public double getValorDiaria() {
+    return valorDiaria;
+  }
+
+  /**
+   * 
+   * @return
+   */
+  
   public int getDiaInicial() {
     return diaInicial;
   }
@@ -120,10 +154,17 @@ public abstract class Quarto implements Servicos, Serializable  {
 
   public void setQuantidadeDePessoas(int pessoas) throws QuantidadeDePessoasInvalidaException {
     verificaQuantidadeDePessoas(pessoas);
-	this.quantidadeDePessoas = pessoas;
+    this.quantidadeDePessoas = pessoas;
   }
-
-  public void verificaQuantidadeDePessoas(int quantidadeDePessoas) throws QuantidadeDePessoasInvalidaException {
+  
+  /**
+   * 
+   * @param quantidadeDePessoas
+   * @throws QuantidadeDePessoasInvalidaException
+   */
+  
+  public void verificaQuantidadeDePessoas(int quantidadeDePessoas) 
+      throws QuantidadeDePessoasInvalidaException {
     if ( quantidadeDePessoas <= 0 || quantidadeDePessoas > 3 ) {
       throw new QuantidadeDePessoasInvalidaException("Quantidade de pessoas do quarto invalida");
     }
