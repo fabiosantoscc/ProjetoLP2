098e73bc46e4d7eab9bde0cc3b984efea7a7159d
fabio <fabio.junior@ccc.ufcg.edu.br>
2015-03-05 01:19:48 -0300

    -

--- ProjetoLP2/src/excecoes/QuartoEsgotadoNoHotelException.java
+++ ProjetoLP2/src/excecoes/QuartoEsgotadoNoHotelException.java
@@ -2,6 +2,8 @@ package excecoes;
 
 public class QuartoEsgotadoNoHotelException extends Exception {
 
+  private static final long serialVersionUID = 1L;
+
   public QuartoEsgotadoNoHotelException(String comentario) {
     super(comentario);
   }
