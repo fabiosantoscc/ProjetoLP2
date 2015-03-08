e1b118d0425899af7250ca773f7ef30748d0393a
fabio <fabio.junior@ccc.ufcg.edu.br>
2015-03-08 02:29:24 -0300

    -

--- ProjetoLP2/src/classes/Restaurante.java
+++ ProjetoLP2/src/classes/Restaurante.java
@@ -2,13 +2,12 @@ package classes;
 
 import java.io.Serializable;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 /**
  * Classe que representa um restaurante
  * 
  * @author Ronan Souza
- * Ultima atualizacao 14/01/2015 / Fabio Alexandre
+ * Ultima atualizacao 08/03/2015 / Fabio Alexandre
  */
 
 
@@ -20,19 +19,17 @@ public class Restaurante implements Servico, Serializable {
 	private UnidadesDoRestaurante unidadeRestaurante;
 	private Calendar data;
 	
-	public Restaurante(UnidadesDoRestaurante tipo, double valorConsumido) throws Exception{
+	public Restaurante(UnidadesDoRestaurante tipo, double valorConsumido) throws Exception {
 		testaUnidadeRestaurante(tipo);
 		testaValorConsumo(valorConsumido);
 		Hotel.maisRestaurante++;
-		unidadeRestaurante = tipo;
+		this.unidadeRestaurante = tipo;
 		calendario = new CalendarioDeEstrategias();
 		data = Calendar.getInstance(); 
 		estrategia = calendario.verificaEstrategia(data.get(Calendar.DAY_OF_MONTH), data.get(Calendar.MONTH) + 1);
 		consumoRefeicao = estrategia.calculaMontante(valorConsumido);
 	}
-	
 
-	
 	/**
 	 * Adiciona ao valor geral o consumo diario de acordo com a estrategia usada naquele dia
 	 * @param dia Dia do consumo
@@ -45,8 +42,8 @@ public class Restaurante implements Servico, Serializable {
 		}	
 	}
 	
-	private void testaUnidadeRestaurante(UnidadesDoRestaurante tipo) throws Exception{
-		if (!(tipo == UnidadesDoRestaurante.TERREO || tipo == UnidadesDoRestaurante.COBERTURA)){
+	private void testaUnidadeRestaurante(UnidadesDoRestaurante tipo) throws Exception {
+		if ( !(tipo == UnidadesDoRestaurante.TERREO || tipo == UnidadesDoRestaurante.COBERTURA) ) {
 			throw new Exception("Unidade do restaurante invalida!!");
 		}
 	}
@@ -56,25 +53,27 @@ public class Restaurante implements Servico, Serializable {
 		return consumoRefeicao;
 	}
 
-	public UnidadesDoRestaurante getUnidadeRestaurante(){ return this.unidadeRestaurante;}
+	public UnidadesDoRestaurante getUnidadeRestaurante() {
+		return unidadeRestaurante;
+	}
+	
 	@Override
 	public String toString() {
-		return "Restaurante:"+Arquivos.FIM_LINHA+
-				"  Valor Consumido: "+ consumoRefeicao+Arquivos.FIM_LINHA+
-				"  Unidade do Restaurante: "+unidadeRestaurante+Arquivos.FIM_LINHA+
-				"  Data do Consumo: "+data.get(Calendar.DAY_OF_MONTH)+"/"+(data.get(Calendar.MONTH) + 1)+"/"+data.get(Calendar.YEAR);
+		return "Restaurante:" + Arquivos.FIM_LINHA
+				+ "  Valor Consumido: "+ consumoRefeicao + Arquivos.FIM_LINHA
+				+ "  Unidade do Restaurante: " + unidadeRestaurante + Arquivos.FIM_LINHA
+				+ "  Data do Consumo: " + data.get(Calendar.DAY_OF_MONTH)
+				+ "/" + (data.get(Calendar.MONTH) + 1) + "/" + data.get(Calendar.YEAR);
 	} 
 	
 	@Override
 	public boolean equals( Object obj ) {
-		if (! (obj instanceof Restaurante)){
+		if (! (obj instanceof Restaurante)) {
 			return false;
 		}
+		
 		Restaurante novo = (Restaurante) obj;
 		
-		if (novo.getDespesaTotal() == consumoRefeicao && novo.getUnidadeRestaurante() == this.unidadeRestaurante){
-			return true;
-		}
-		return false;
+		return novo.getDespesaTotal() == consumoRefeicao && novo.getUnidadeRestaurante() == unidadeRestaurante;
 	}
 }
