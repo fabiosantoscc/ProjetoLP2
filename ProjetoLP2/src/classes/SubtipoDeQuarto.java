d824b60c61d4d74317551a935ba0115ceb025f5b
fabio <fabio.junior@ccc.ufcg.edu.br>
2015-03-09 19:56:15 -0300

    SubTipoDeQuarto

--- ProjetoLP2/src/classes/SubtipoDeQuarto.java
+++ ProjetoLP2/src/classes/SubtipoDeQuarto.java
@@ -1,12 +1,12 @@
 package classes;
 
-public enum SubtipoQuartoExecutivo {
+public enum SubtipoDeQuarto {
 
-	SIMPLES(360), DUPLO(385), TRIPLO(440);
+	SIMPLES(360), DUPLO(385), TRIPLO(440), SIMPLES2(520), DUPLO2(570), TRIPLO2(620);
 	
 	private double valorDiaria;
 	
-	SubtipoQuartoExecutivo( double valorDiaria ) {
+	SubtipoDeQuarto( double valorDiaria ) {
 		this.valorDiaria = valorDiaria;
 	}
 	
