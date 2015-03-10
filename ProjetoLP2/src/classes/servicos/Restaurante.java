d43327b608b8f45b78c8e612d780f7c46307a621
fabio <fabio.junior@ccc.ufcg.edu.br>
2015-03-10 13:48:41 -0300

    Alteração no toString de restaurante. (RestauranteTest não testa todos os casos de teste).

--- ProjetoLP2/src/classes/servicos/Restaurante.java
+++ ProjetoLP2/src/classes/servicos/Restaurante.java
@@ -3,7 +3,6 @@ package classes.servicos;
 import java.io.Serializable;
 import java.util.Calendar;
 
-import classes.Arquivos;
 import classes.CalendarioDeEstrategias;
 import classes.EstrategiaDeCalculoDaMontante;
 import classes.Hotel;
@@ -12,11 +11,12 @@ import classes.Hotel;
  * Classe que representa um restaurante
  * 
  * @author Ronan Souza
- * Ultima atualizacao 08/03/2015 / Fabio Alexandre
+ * Ultima atualizacao 10/03/2015 / Fabio Alexandre
  */
 
 
 public class Restaurante implements Servico, Serializable {
+	
 	private CalendarioDeEstrategias calendario;
 	private static final long serialVersionUID = 1L;
 	private EstrategiaDeCalculoDaMontante estrategia;
@@ -27,10 +27,10 @@ public class Restaurante implements Servico, Serializable {
 	public Restaurante(UnidadesDoRestaurante tipo, double valorConsumido) throws Exception {
 		testaUnidadeRestaurante(tipo);
 		testaValorConsumo(valorConsumido);
-		Hotel.maisRestaurante++;
 		this.unidadeRestaurante = tipo;
 		calendario = new CalendarioDeEstrategias();
-		data = Calendar.getInstance(); 
+		data = Calendar.getInstance();
+		Hotel.maisRestaurante++;
 		estrategia = calendario.verificaEstrategia(data.get(Calendar.DAY_OF_MONTH), data.get(Calendar.MONTH) + 1);
 		consumoRefeicao = estrategia.calculaMontante(valorConsumido);
 	}
@@ -41,8 +41,8 @@ public class Restaurante implements Servico, Serializable {
 	 * @param mes Mes do consumo
 	 */
 	
-	private void testaValorConsumo(double valor) throws Exception{
-		if (valor <= 0){
+	private void testaValorConsumo(double valor) throws Exception {
+		if ( valor <= 0 ) {
 			throw new Exception("Valor invalido!!");
 		}	
 	}
@@ -64,10 +64,9 @@ public class Restaurante implements Servico, Serializable {
 	
 	@Override
 	public String toString() {
-		return "Restaurante:" + Arquivos.FIM_LINHA
-				+ "  Valor Consumido: "+ consumoRefeicao + Arquivos.FIM_LINHA
-				+ "  Unidade do Restaurante: " + unidadeRestaurante + Arquivos.FIM_LINHA
-				+ "  Data do Consumo: " + data.get(Calendar.DAY_OF_MONTH)
+		return "Restaurante: Valor Consumido: " + getDespesaTotal()
+				+ ", Unidade do Restaurante: " + getUnidadeRestaurante().name().toLowerCase()
+				+ ", Data do Consumo: " + data.get(Calendar.DAY_OF_MONTH)
 				+ "/" + (data.get(Calendar.MONTH) + 1) + "/" + data.get(Calendar.YEAR);
 	} 
 	
