4b559bbd13c20f929bbc67c19071371a1f511ba3
fabio <fabio.junior@ccc.ufcg.edu.br>
2015-03-10 23:20:37 -0300

    -

--- ProjetoLP2/src/classes/servicos/AluguelDeCarros.java
+++ ProjetoLP2/src/classes/servicos/AluguelDeCarros.java
@@ -1,32 +1,30 @@
 package classes.servicos;
 
-import java.io.Serializable;
-import java.util.Calendar;
-
-import classes.Arquivos;
 import classes.Hotel;
 import classes.estrategias.CalendarioDeEstrategias;
 import classes.estrategias.EstrategiaDeCalculoDaMontante;
 import enums.ModelosDoCarro;
+
+import java.io.Serializable;
+import java.util.Calendar;
 
 /**
  * Classe que recebe informações a cerca do aluguel de automoveis para clientes do Hotel e faz o gerenciamento desses dados.
  * 
  * @author Ronan Souza
  * @date 12/01/2014
- * Ultima Alteracao : FAbio Alexandre 10/02/2015
+ * Ultima Alteracao / Fabio Alexandre 10/02/2015
  */
  
 public class AluguelDeCarros implements Servico, Serializable {
-	private EstrategiaDeCalculoDaMontante estrategia;
 	private static final long serialVersionUID = 1L;
-
+	private EstrategiaDeCalculoDaMontante estrategia;
 	private CalendarioDeEstrategias calendario;
 	private ModelosDoCarro modeloCarro;
 	private boolean tanqueCheio;
 	private boolean seguroDeAutomovel;
 	private int valorDiaria;
-	private int tarifaTotal = 0;
+	private int tarifaTotal;
 	private Calendar data;
 	
 	/**
@@ -39,16 +37,16 @@ public class AluguelDeCarros implements Servico, Serializable {
 	 */
 	
 	public AluguelDeCarros(ModelosDoCarro carro, boolean tanqueCheio, boolean seguroAutomovel) throws Exception {
-		Hotel.maisAluguel++;
 		checaTipoCarro(carro);
 		data = Calendar.getInstance();		
 		this.modeloCarro = carro;
 		this.tanqueCheio = tanqueCheio;
 		this.seguroDeAutomovel = seguroAutomovel;
+		Hotel.maisAluguel++;
 	}	
 	
 	private void checaTipoCarro(ModelosDoCarro carro) throws Exception {
-		if (!( carro.equals(ModelosDoCarro.EXECUTIVO)||  carro.equals(ModelosDoCarro.LUXO))){
+		if (!( carro.equals(ModelosDoCarro.EXECUTIVO)||  carro.equals(ModelosDoCarro.LUXO))) {
 			throw new Exception("Modelo de Automóvel Invalido");
 		}
 	}
@@ -60,10 +58,6 @@ public class AluguelDeCarros implements Servico, Serializable {
 	
 	public ModelosDoCarro getModeloCarro() {
 		return this.modeloCarro;
-	}
-	
-	public double getPreco() { 
-		return tarifaTotal;
 	}
 	
 	/**
@@ -79,7 +73,7 @@ public class AluguelDeCarros implements Servico, Serializable {
 	 * @return O valor da diaria dependendo do tipo
 	 */
 	
-	public double getValorDiaria(){
+	public double getValorDiaria() {
 		return valorDiaria;
 	}
 
@@ -97,8 +91,13 @@ public class AluguelDeCarros implements Servico, Serializable {
 	 */
 	
 	public void verificaTipo() {
-		if (this.modeloCarro.equals(ModelosDoCarro.LUXO)) valorDiaria = 100;
-		else if (this.modeloCarro.equals(ModelosDoCarro.EXECUTIVO)) valorDiaria = 60;
+		if (this.modeloCarro.equals(ModelosDoCarro.LUXO)) {
+			valorDiaria = 100;
+		}
+		
+		else if (this.modeloCarro.equals(ModelosDoCarro.EXECUTIVO)) {
+			valorDiaria = 60;
+		}
 	}
 	
 	/**
@@ -123,12 +122,12 @@ public class AluguelDeCarros implements Servico, Serializable {
 	 */
 
 	public int calculaDespesaTotal(int diaEntrada, int mesEntrada, int diaSaida, int mesSaida, int anoEntrada,
-			int anoSaida)throws Exception{
+			int anoSaida) throws Exception {
 		if (!calendario.verificaDataValida(diaEntrada, mesEntrada)) throw new Exception ("O mes e o dia tem que ser valido.");
 		if (!calendario.verificaDataValida(diaSaida, mesSaida)) throw new Exception ("O mes e o dia tem que ser valido.");
 		boolean dataValida = true;
 		int contador = 0;
-		while (dataValida){
+		while (dataValida) {
 			if (diaEntrada > diaSaida && mesEntrada == mesSaida && anoEntrada == anoSaida) break;
 			if (!(calendario.verificaDataValida(diaEntrada, mesEntrada))){
 				diaEntrada = 1;
@@ -141,53 +140,63 @@ public class AluguelDeCarros implements Servico, Serializable {
 			contador++;
 			estrategia = calendario.verificaEstrategia(diaEntrada, mesEntrada);
 			tarifaTotal += estrategia.calculaMontante(valorDiaria);
-			System.out.println(tarifaTotal);
 			diaEntrada++;
 		}
 		return contador; 
 	}
 	
 		
-	/** 
-	 * Metodo que retorna uma String com os atributos da classe
-	 */
-	@Override
-	public String toString() {
-		String string;
-		string = "Aluguel de Carro - "+ Arquivos.FIM_LINHA+
-		"  Modelo: "+this.modeloCarro+Arquivos.FIM_LINHA+		
-		"  Data: " + data.get(Calendar.DAY_OF_WEEK) + "/" + data.get(Calendar.MONTH + 1)+ "/"
-				+ data.get(Calendar.YEAR) + " às " + data.get(Calendar.HOUR_OF_DAY) + ":" + data.get(Calendar.MINUTE) + ":" + data.get(Calendar.SECOND);
-		
-		if ( tanqueCheio ) {
-			string += ", Tanque cheio";
-		} else {
-			string += ", Sem tanque cheio";
-		}
-		
-		if ( seguroDeAutomovel ) {
-			string += ", Carro com seguro.";
-		} else {
-			string += ", Carro sem seguro.";
-		}
-		
-		return string;
-	}
+  /** 
+  * Metodo que retorna uma String com os atributos da classe
+  */
 
-	@Override
-	public boolean equals( Object obj ) {
-		if (!(obj instanceof AluguelDeCarros))
-			return false;
-		
-		AluguelDeCarros novoAluguel = (AluguelDeCarros)obj;
-		
-		return (modeloCarro.equals(novoAluguel.getModeloCarro()) && tanqueCheio == novoAluguel.isTanqueCheio() &&
-				seguroDeAutomovel == novoAluguel.isSeguroDeAutomovel());
-		}
+  @Override
+  public String toString() {
+    String string;
+    string = "Aluguel de Carro - Modelo: " + getModeloCarro().name().toLowerCase()		
+        + "  Data: " + data.get(Calendar.DAY_OF_WEEK) + "/" + data.get(Calendar.MONTH + 1) + "/"
+        + data.get(Calendar.YEAR) + " às " + data.get(Calendar.HOUR_OF_DAY) + ":" + data.get(Calendar.MINUTE) + ":" + data.get(Calendar.SECOND);
 
-	@Override
-	public double getDespesaTotal() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    if ( tanqueCheio ) {
+      string += ", Tanque cheio";
+    } else {
+      string += ", Sem tanque cheio";
+    }
+
+    if ( seguroDeAutomovel ) {
+      string += ", Carro com seguro.";
+    } else {
+      string += ", Carro sem seguro.";
+    }
+
+    return string;
+  }
+
+  /**
+   * 
+   */
+  
+  @Override
+  public double getDespesaTotal() {
+    return tarifaTotal;
+  }
+	
+  /**
+  *
+  * 
+  * @return
+  */
+
+  @Override
+  public boolean equals( Object obj ) {
+    if (! (obj instanceof AluguelDeCarros) ) {
+      return false;
+    }
+
+    AluguelDeCarros novoAluguel = (AluguelDeCarros)obj;
+
+    return modeloCarro.equals(novoAluguel.getModeloCarro()) &&
+        tanqueCheio == novoAluguel.isTanqueCheio()
+        && seguroDeAutomovel == novoAluguel.isSeguroDeAutomovel();
+  }
 }
