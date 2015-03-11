a139d5f6aad9e8934e7b62a8529627c69a6c1a03
Ronan <Ronan@Ronan-PC>
2015-03-11 00:18:14 -0300

    Refatoramento AdicionarHospede

--- ProjetoLP2/src/interfaceGrafica/AdicionarHospede.java
+++ ProjetoLP2/src/interfaceGrafica/AdicionarHospede.java
@@ -1,6 +1,7 @@
 package interfaceGrafica;
 
 import java.awt.Font;
+import java.awt.Rectangle;
 
 import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
@@ -8,6 +9,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.JTextField;
 
 import classes.Arquivos;
 import classes.dadosDoHospede.Endereco;
@@ -26,160 +28,115 @@ import javax.swing.ImageIcon;
 
 public class AdicionarHospede extends JPanel {
 	
-	Calendar dataNascimento;
+	private Calendar dataNascimento;
+	private JLabel lblAtualizarCadastro, lblDadosPessoais, lblNome, lblCPF, lblEmail, lblCartao, lblTelefone, lblEstado, lblNumeroCasa,
+	lblCepZipCode, lblCidade, lblEndereco, lblLogradouro, lblComplemento, lblBairro, lblPais, lblData;
+	private JTextField textEmail, txtCartao, textDDD, textNumero, textNome, textRua, textComplemento, textBairro, textPais, textCidade, 
+	textCep, textNumeroCasa, textEstado, textCPF;
+	private JSeparator separator;
+	private JButton btnCadastrar;
 	
-	/**
-	 * Create the panel.
-	 */
 	public AdicionarHospede() {
 
 		setLayout(null);
 		
-		JLabel lblAtualizarCadastro = new JLabel("Adicionar Hospede");
-		lblAtualizarCadastro.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblAtualizarCadastro.setBounds(12, 12, 287, 29);
+		lblAtualizarCadastro = CriaObjetosNaTela.criaLabel("Adicionar Hospede", new Rectangle(12, 12, 287, 29), CriaObjetosNaTela.getFontePadrao(24), null, null);
 		add(lblAtualizarCadastro);
 		
-		JLabel lblDadosPessoais = new JLabel("Dados Pessoais");
-		lblDadosPessoais.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblDadosPessoais.setBounds(38, 53, 174, 29);
+		lblDadosPessoais = CriaObjetosNaTela.criaLabel("Dados Pessoais", new Rectangle(38, 53, 174, 29), CriaObjetosNaTela.getFontePadrao(17), null, null);
 		add(lblDadosPessoais);
 		
-		JLabel lblNome = new JLabel("Nome");
-		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNome.setBounds(68, 91, 70, 25);
+		lblNome = CriaObjetosNaTela.criaLabel("Nome", new Rectangle(68, 91, 70, 25), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblNome);
 		
-		JLabel lblCPF = new JLabel("CPF");
-		lblCPF.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblCPF.setBounds(68, 128, 70, 15);
+		lblCPF = CriaObjetosNaTela.criaLabel("CPF", new Rectangle(68, 128, 70, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblCPF);
 		
-		JLabel lblEmail = new JLabel("Email");
-		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblEmail.setBounds(68, 155, 70, 15);
+		lblEmail = CriaObjetosNaTela.criaLabel("Email", new Rectangle(68, 155, 70, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblEmail);
 		
 		
-		JLabel lblCartao = new JLabel("Cartao de credito");
-		lblCartao.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblCartao.setBounds(68, 182, 158, 15);
+		lblCartao = CriaObjetosNaTela.criaLabel("Cartao de credito", new Rectangle(68, 182, 158, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblCartao);
 		
-		JFormattedTextField textNome = new JFormattedTextField();
-		textNome.setBounds(119, 99, 440, 17);
+		textNome = CriaObjetosNaTela.criaTextField(null, new Rectangle(119, 99, 440, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textNome);
 		
-		JFormattedTextField textEmail = new JFormattedTextField();
-		textEmail.setBounds(119, 155, 264, 17);
+		textEmail = CriaObjetosNaTela.criaTextField(null, new Rectangle(119, 155, 264, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textEmail);
 		
-		JFormattedTextField txtCartao = new JFormattedTextField();
-		txtCartao.setBounds(203, 182, 151, 17);
+		txtCartao = CriaObjetosNaTela.criaTextField(null, new Rectangle(203, 182, 151, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(txtCartao);
 		
-		JLabel lblTelefone = new JLabel("Telefone");
-		lblTelefone.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblTelefone.setBounds(313, 129, 70, 15);
+		lblTelefone = CriaObjetosNaTela.criaLabel("Telefone", new Rectangle(313, 129, 70, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblTelefone);
 		
-		JFormattedTextField textDDD = new JFormattedTextField();
-		textDDD.setBounds(388, 128, 44, 17);
+		textDDD = CriaObjetosNaTela.criaTextField(null, new Rectangle(388, 128, 44, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textDDD);
 		
-		JFormattedTextField textNumero = new JFormattedTextField();
-		textNumero.setBounds(445, 128, 114, 17);
+		textNumero = CriaObjetosNaTela.criaTextField(null, new Rectangle(445, 128, 114, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textNumero);
 		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(38, 240, 687, 2);
+		separator = CriaObjetosNaTela.criaSeparator(new Rectangle(38, 240, 687, 2));
 		add(separator);
 		
-		JLabel lblEstado = new JLabel("Estado/Condado");
-		lblEstado.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblEstado.setBounds(58, 420, 126, 15);
+		lblEstado = CriaObjetosNaTela.criaLabel("Estado/Condado", new Rectangle(58, 420, 126, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblEstado);
 		
-		JLabel lblNumeroCasa = new JLabel("Numero");
-		lblNumeroCasa.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNumeroCasa.setBounds(476, 329, 70, 15);
+		lblNumeroCasa = CriaObjetosNaTela.criaLabel("Numero", new Rectangle(476, 329, 70, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblNumeroCasa);
 		
-		JLabel lblCepZipCode = new JLabel("CEP/ Zip Code");
-		lblCepZipCode.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblCepZipCode.setBounds(292, 395, 126, 15);
+		lblCepZipCode = CriaObjetosNaTela.criaLabel("CEP/ Zip Code", new Rectangle(292, 395, 126, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblCepZipCode);
 		
-		JLabel lblCidade = new JLabel("Cidade");
-		lblCidade.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblCidade.setBounds(292, 363, 70, 15);
+		lblCidade = CriaObjetosNaTela.criaLabel("Cidade", new Rectangle(292, 363, 70, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblCidade);
 		
-		JLabel lblEndereo = new JLabel("Endere\u00E7o");
-		lblEndereo.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblEndereo.setBounds(38, 254, 114, 25);
-		add(lblEndereo);
+		lblEndereco = CriaObjetosNaTela.criaLabel("Endere\u00E7o", new Rectangle(38, 254, 114, 25), CriaObjetosNaTela.getFontePadrao(17), null, null);
+		add(lblEndereco);
 		
-		JLabel lblLogradouro = new JLabel("Logradouro");
-		lblLogradouro.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblLogradouro.setBounds(58, 302, 138, 15);
+		lblLogradouro = CriaObjetosNaTela.criaLabel("Logradouro", new Rectangle(58, 302, 138, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblLogradouro);
 		
-		JLabel lblComplemento = new JLabel("Complemento");
-		lblComplemento.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblComplemento.setBounds(58, 329, 114, 15);
+		lblComplemento = CriaObjetosNaTela.criaLabel("Complemento", new Rectangle(58, 329, 114, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblComplemento);
 		
-		JLabel lblBairro = new JLabel("Bairro");
-		lblBairro.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblBairro.setBounds(58, 361, 70, 15);
+		lblBairro = CriaObjetosNaTela.criaLabel("Bairro", new Rectangle(58, 361, 70, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblBairro);
 		
-		JLabel lblPais = new JLabel("Pais");
-		lblPais.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblPais.setBounds(58, 394, 70, 15);
+		lblPais = CriaObjetosNaTela.criaLabel("Pais", new Rectangle(58, 394, 70, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblPais);
 		
-		JFormattedTextField textRua = new JFormattedTextField();
-		textRua.setBounds(144, 302, 468, 17);
+		textRua = CriaObjetosNaTela.criaTextField(null, new Rectangle(144, 302, 468, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textRua);
 		
-		JFormattedTextField textComplemento = new JFormattedTextField();
-		textComplemento.setBounds(167, 329, 299, 17);
+		textComplemento = CriaObjetosNaTela.criaTextField(null, new Rectangle(167, 329, 299, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textComplemento);
 		
-		JFormattedTextField textBairro = new JFormattedTextField();
-		textBairro.setBounds(119, 361, 138, 17);
+		textBairro = CriaObjetosNaTela.criaTextField(null, new Rectangle(119, 361, 138, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textBairro);
 		
-		JFormattedTextField textPais = new JFormattedTextField();
-		textPais.setBounds(119, 394, 138, 17);
+		textPais = CriaObjetosNaTela.criaTextField(null , new Rectangle(119, 394, 138, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textPais);
 		
-		JFormattedTextField textCidade = new JFormattedTextField();
-		textCidade.setBounds(376, 361, 236, 17);
+		textCidade = CriaObjetosNaTela.criaTextField(null, new Rectangle(376, 361, 236, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textCidade);
 		
-		JFormattedTextField textCep = new JFormattedTextField();
-		textCep.setBounds(436, 393, 138, 17);
+		textCep = CriaObjetosNaTela.criaTextField(null, new Rectangle(436, 393, 138, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textCep);
 		
-		JFormattedTextField textNumeroCasa = new JFormattedTextField();
+		textNumeroCasa = CriaObjetosNaTela.criaTextField(null, new Rectangle(542, 329, 70, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		textNumeroCasa.setBounds(542, 329, 70, 17);
 		add(textNumeroCasa);
 		
-		JFormattedTextField textEstado = new JFormattedTextField();
-		textEstado.setBounds(203, 422, 180, 17);
+		textEstado = CriaObjetosNaTela.criaTextField(null, new Rectangle(203, 422, 180, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textEstado);
 		
-		JFormattedTextField textCPF = new JFormattedTextField();
-		textCPF.setBounds(119, 127, 151, 17);
+		textCPF = CriaObjetosNaTela.criaTextField(null, new Rectangle(119, 127, 151, 17), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textCPF);
 		
 		
-		JButton btnCadastrar = new JButton("Criar");
-		btnCadastrar.setIcon(new ImageIcon(AdicionarHospede.class.getResource("/Icons/accept.png")));
-		btnCadastrar.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnCadastrar = CriaObjetosNaTela.criaBotao("Criar", new Rectangle(334, 497, 138, 29), CriaObjetosNaTela.getFontePadrao(17),new ImageIcon(AdicionarHospede.class.getResource("/Icons/accept.png")), null);
 		btnCadastrar.addActionListener(new ActionListener()  {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
@@ -190,20 +147,6 @@ public class AdicionarHospede extends JPanel {
 					Run.hotel.addHospede(h);
 					Arquivos.salvaHotel(Run.hotel);
 					JOptionPane.showMessageDialog(null, "Hospede criado com sucesso!");
-					textRua.setText("");
-					textComplemento.setText("");
-					textNumeroCasa.setText("");
-					textBairro.setText("");
-					textCidade.setText("");
-					textPais.setText("");
-					textEstado.setText("");
-					textCep.setText("");
-					textNome.setText("");
-					textCPF.setText("");
-					textEmail.setText("");
-					txtCartao.setText("");
-					textDDD.setText("");
-					textNumero.setText("");
 					MenuPrincipal.cl.show(MenuPrincipal.panel0, "MenuPadrao");
 					
 				} catch (Exception e) {
@@ -211,7 +154,6 @@ public class AdicionarHospede extends JPanel {
 				}
 			}
 		});
-		btnCadastrar.setBounds(334, 497, 138, 29);
 		add(btnCadastrar);
 		
 		JDateChooser dateChooser = new JDateChooser();
@@ -223,9 +165,7 @@ public class AdicionarHospede extends JPanel {
 		dateChooser.setBounds(445, 182, 114, 17);
 		add(dateChooser);
 		
-		JLabel lblData = new JLabel("Data de nascimento");
-		lblData.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblData.setBounds(421, 155, 138, 15);
+		lblData = CriaObjetosNaTela.criaLabel("Data de nascimento", new Rectangle(421, 155, 138, 15), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblData);
 	}
 }
