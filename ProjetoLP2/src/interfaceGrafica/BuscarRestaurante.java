5011249646cbc07d1bd2244b472c11d5ddb95eb9
Ronan <Ronan@Ronan-PC>
2015-03-12 05:30:03 -0300

    \

--- ProjetoLP2/src/interfaceGrafica/BuscarRestaurante.java
+++ ProjetoLP2/src/interfaceGrafica/BuscarRestaurante.java
@@ -22,37 +22,28 @@ import executar.Run;
 
 
 public class BuscarRestaurante extends JPanel {
+	
+	private JButton btnBuscar;
 	private JTextField textCPF;
-
-	/**
-	 * Create the panel.
-	 */
-	List<Servico> servicosAtuais = null;
+	private JLabel lblLogoHotel,lblBabySitter, lblCPF;
+	private List<Servico> servicosAtuais = null;
+	
 	public BuscarRestaurante() {
-		setLayout(null);
+setLayout(null);
 		
-		JLabel lblLogoHotel = CriaObjetosNaTela.criaLabel(null, new Rectangle(694, 11, 66, 70), CriaObjetosNaTela.getFontePadrao(20),new ImageIcon(TelaSobre.class.getResource("/Icons/60x53.png")), null);
+		lblLogoHotel = CriaObjetosNaTela.criaLabel(null, new Rectangle(694, 11, 66, 70), CriaObjetosNaTela.getFontePadrao(20),new ImageIcon(TelaSobre.class.getResource("/Icons/60x53.png")), null);
 		add(lblLogoHotel);
 		
-		JLabel lblRestaurante = new JLabel("Restaurante");
-		lblRestaurante.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblRestaurante.setBounds(38, 29, 175, 40);
-		add(lblRestaurante);
+		lblBabySitter = CriaObjetosNaTela.criaLabel("BabySitter", new Rectangle(38, 29, 329, 40), CriaObjetosNaTela.getFontePadrao(24), null, null);
+		add(lblBabySitter);
 		
-		JLabel lblCPF = new JLabel("CPF do Hospede");
-		lblCPF.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblCPF.setBounds(204, 136, 114, 21);
+		lblCPF = CriaObjetosNaTela.criaLabel("CPF do Hospede", new Rectangle(216, 132, 116, 21), CriaObjetosNaTela.getFontePadrao(15), null, null);
 		add(lblCPF);
 		
-		textCPF = new JTextField();
-		textCPF.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		textCPF.setColumns(10);
-		textCPF.setBounds(344, 136, 136, 20);
+		textCPF = CriaObjetosNaTela.criaTextField(null, new Rectangle(381, 132, 148, 20), CriaObjetosNaTela.getFontePadrao(15), true, null);
 		add(textCPF);
 		
-		JButton btnBuscar = new JButton("Buscar");
-		btnBuscar.setIcon(new ImageIcon(BuscarRestaurante.class.getResource("/Icons/zoom.png")));
-		btnBuscar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnBuscar = CriaObjetosNaTela.criaBotao("Buscar", new Rectangle(335, 191, 105, 27), CriaObjetosNaTela.getFontePadrao(15),new ImageIcon(BuscarAdcBabySitter.class.getResource("/Icons/zoom.png")) , null);
 		btnBuscar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try{
@@ -65,7 +56,6 @@ public class BuscarRestaurante extends JPanel {
 				}
 			}
 		});
-		btnBuscar.setBounds(297, 192, 106, 29);
 		add(btnBuscar);
 
 	}
